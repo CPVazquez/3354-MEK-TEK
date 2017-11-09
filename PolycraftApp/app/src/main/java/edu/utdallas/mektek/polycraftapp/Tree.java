82400a66d218d2ee0858731ee82af3a3e3a612b2
ChandranshuRao14 <chandranshu.rao@gmail.com>
2017-11-09 17:30:52 -0600

    Added Tree class

---
+++ PolycraftApp/app/src/main/java/edu/utdallas/mektek/polycraftapp/Tree.java
@@ -0,0 +1,28 @@
+package edu.utdallas.mektek.polycraftapp;
+
+import java.util.ArrayList;
+
+public class Tree {
+
+    private SuperNode targetNode;
+
+    public Tree(String itemID){
+
+    }
+
+    public void addNode(SuperNode node){
+
+    }
+
+    public void deleteNode(SuperNode node){
+
+    }
+
+    public SuperNode getNode(String itemID){
+        return null;
+    }
+
+    public ArrayList<SuperNode> getDrawnNodes(){
+        return null;
+    }
+}
