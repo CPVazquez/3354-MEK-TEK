77626e1fe3f573e853a4475934d936c506cde595
ChandranshuRao14 <chandranshu.rao@gmail.com>
2017-11-13 13:18:20 -0600

    Rewrote Tree class's methods and edited main activity xml

--- PolycraftApp/app/src/main/java/edu/utdallas/mektek/polycraftapp/Tree.java
+++ PolycraftApp/app/src/main/java/edu/utdallas/mektek/polycraftapp/Tree.java
@@ -5,24 +5,55 @@ import java.util.ArrayList;
 public class Tree {
 
     private SuperNode targetNode;
+    private SuperNode pointerNode;
 
-    public Tree(String itemID){
+    public Tree(SuperNode target){
 
+        this.targetNode = target;
+        this.pointerNode = null;
     }
 
-    public void addNode(SuperNode node){
+    // Attach this node to pointerNode
+    public void addNode(SuperNode nodeToAdd){
 
+        // This tree was just created, add the node to target
+        if(this.pointerNode == null){
+            // Make parent and child connection
+            for (SuperNode parent : nodeToAdd.getParents()){
+                if(parent.getId().equals(this.targetNode.getId())){
+                    // Make parent/child connection
+                    this.targetNode = parent;
+                    this.pointerNode = nodeToAdd;
+                    break; // We done
+                }
+            }
+        }
+        else{
+            // Loop through pointerNode's children
+            for (SuperNode child : this.pointerNode.getChildren()) {
+                // Loop through nodeToAdd's parents
+                for (SuperNode parent : nodeToAdd.getParents()) {
+                    // Parent and child IDs match we make the connection here
+                    if(child.getId().equals(parent.getId())){
+                        
+                        this.pointerNode = nodeToAdd;
+                        break;
+                    }
+                }
+            }
+        }
     }
 
     public void deleteNode(SuperNode node){
-
+        // Unimplemented
     }
 
-    public SuperNode getNode(String itemID){
+    public ArrayList<SuperNode> getDrawnNodes()
+    {
         return null;
     }
 
-    public ArrayList<SuperNode> getDrawnNodes(){
-        return null;
+    public SuperNode getTargetNode(){
+        return this.targetNode;
     }
 }
