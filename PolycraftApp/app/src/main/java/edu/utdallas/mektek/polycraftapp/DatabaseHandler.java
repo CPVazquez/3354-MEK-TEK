6c638f8c473e3393d4e8b08883235afea54b45ff
Keene Chin <trix1011@gmail.com>
2017-12-02 18:51:18 -0600

    removed deprecated  queryDistillRecipeData method
    Replaced by queryDistillRecipeRowID and querySpecific RecipeDetails

--- PolycraftApp/app/src/main/java/edu/utdallas/mektek/polycraftapp/DatabaseHandler.java
+++ PolycraftApp/app/src/main/java/edu/utdallas/mektek/polycraftapp/DatabaseHandler.java
@@ -292,5 +292,4 @@ public class DatabaseHandler extends SQLiteAssetHelper{
         }
     }//ItemList Class
 
-
 }
--- PolycraftApp/app/src/main/java/edu/utdallas/mektek/polycraftapp/SQLquery.java
+++ PolycraftApp/app/src/main/java/edu/utdallas/mektek/polycraftapp/SQLquery.java
@@ -7,19 +7,6 @@ public final class SQLquery {
 	public static final String selectIdsAndNames = "SELECT gameID, itemName FROM iteminfo";
 	public static final  String selectIsNatural = "SELECT itemNatural FROM iteminfo";
 
-	public static final String queryDistillRecipeData(int columns_to_check) {
-		String addendum = " WHERE ";
-		int params = columns_to_check;
-		
-		for(int i = 0; i < params-1; i++){
-			addendum += "output" + (i+1) + " = ? OR ";
-		}
-		addendum += "output" + params + " = ?";
-		String query = SQLquery.selectRecipes + addendum;
-		
-		return query;
-	}
-
 	public static final String queryDistillRecipeRowId(int columns_to_check) {
 		String addendum = " WHERE ";
 		int params = columns_to_check;
