9881321eb8c4d311c8c15100d9eee579d59e64b5
thedhruvn <11063206+thedhruvn@users.noreply.github.com>
2017-12-06 18:01:24 -0600

    Commented all of SQLQuery.java - removed redundant lines.

--- PolycraftApp/app/src/main/java/edu/utdallas/mektek/polycraftapp/SQLquery.java
+++ PolycraftApp/app/src/main/java/edu/utdallas/mektek/polycraftapp/SQLquery.java
@@ -1,12 +1,24 @@
 package edu.utdallas.mektek.polycraftapp;
 
+/**
+ * SQLQuery is a helper class that contains all of the SQLQuery strings used in our databaseHandler calls.
+ * This class also contains static functions that returns parameterized queries.
+ * This class also builds queries and returns them.
+ */
 public final class SQLquery {
 	public static final String selectItems = "SELECT * FROM iteminfo";
 	public static final String selectRecipes = "SELECT * FROM distillrecipe";
 	public static final String selectRowIds = "SELECT rowid, input1 FROM distillrecipe";
-	public static final String selectIdsAndNames = "SELECT gameID, itemName FROM iteminfo";
 	public static final  String selectIsNatural = "SELECT itemNatural FROM iteminfo";
 
+    /**
+     * Query DistillRecipe Table's output columns, returning only the rowId and input1 columns
+     *
+     * @param columns_to_check number of output columns to check (though 9 columns exist, only a
+     *                         maximum of 7 are used currently, in the formulas provided by PolycraftWorld
+     *                         development team)
+     * @return the built SQLQuery with ? where the query value should go.
+     */
 	public static final String queryDistillRecipeRowId(int columns_to_check) {
 		String addendum = " WHERE ";
 		int params = columns_to_check;
@@ -19,14 +31,24 @@ public final class SQLquery {
 		
 		return query;
 	}
-	
+
+    /**
+     * query the distillrecipe table, returning the entire row that matches the input rowID
+     * @param rowId the rowID of the table the user wants back
+     * @return a string containing the proper query to EXACTLY search for the rowID.
+     */
 	public static final String querySpecificRecipeDetails(String rowId) {
 		String query = selectRecipes;
 		query += " WHERE rowid LIKE '" + rowId + "'";
 		query += " LIMIT 1"; //prevent multiple items from being called here.
 		return query;
 	}
-	
+
+    /**
+     * Query the itemdetails table to see if the searched item name is a "naturally occurring" item.
+     * @param searchedItem the name of the item to check
+     * @return a SQLQuery that accomplishes this task.
+     */
 	public static final String queryItemIsNatural(String searchedItem) {
 		
 		String query = selectIsNatural;
@@ -34,7 +56,12 @@ public final class SQLquery {
 		query += " LIMIT 1"; //prevent multiple items from being called here.
 		return query;
 	}
-	
+
+    /**
+     * Get all details of an item in the itemdetail table, passing in the name of the item
+     * @param searchedItem name of the item to get
+     * @return a SQLQuery that accomplishes this task
+     */
 	public static final String queryItemDetails(String searchedItem) {
 		String query = selectItems;
 		query += " WHERE itemName LIKE '%" + searchedItem + "%'";
@@ -42,6 +69,11 @@ public final class SQLquery {
 		return query;
 	}
 
+    /**
+     * get all details of an item in the itemdetail table, passing in the ID of the item
+     * @param gameID the ID of the item we want details on
+     * @return a SQLQuery that can do this.
+     */
 	public static final String queryItemDetailsWithId(String gameID) {
 		String query = selectItems;
 		query += " WHERE gameID LIKE '%" + gameID + "%'";
